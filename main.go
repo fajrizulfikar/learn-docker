7e5f10df8d260b0b4c2dd58aacb398dc92b57aa7
fajri <fajrizulfikar85@gmail.com>
2023-09-05 13:19:00 +0700

    feat: use clear error message

--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 )
 
 func createNewRoot() {
-	os.MkdirAll("/home/fajri/simple_docker/bin", 0755)
-	cmd := exec.Command("cp", "-v", "/bin/ssh", "/home/fajri/simple_docker/bin")
-	err := cmd.Run()
+	err := os.MkdirAll("/home/fajri/simple_docker/bin", 0755)
 	if err != nil {
-		panic(err)
+		panic("Failed to create new root: " + err.Error())
+	}
+	cmd := exec.Command("cp", "-v", "/bin/sh", "/home/fajri/simple_docker/bin")
+	err = cmd.Run()
+	if err != nil {
+		panic("Failed to copy /bin/sh: " + err.Error())
 	}
 }
 
@@ -25,32 +28,46 @@ func main() {
 	const CLONE_NEWNS = 0x00020000
 	const SYS_UNSHARE = 310
 
-	if _, _, err := syscall.Syscall(uintptr(SYS_UNSHARE), uintptr(CLONE_NEWUTS|CLONE_NEWPID|CLONE_NEWNS), 0, 0); err != 0 {
-		panic(err)
+	_, _, errno := syscall.Syscall(uintptr(SYS_UNSHARE), uintptr(CLONE_NEWUTS|CLONE_NEWPID|CLONE_NEWNS), 0, 0)
+	if errno != 0 {
+		panic("Failed to unshare: " + errno.Error())
 	}
 
 	// Create cgroup directory
 	err := os.Mkdir("/sys/fs/cgroup/memory/mydocker", 0755)
 	if err != nil && !os.IsExist(err) {
-		panic(err)
+		panic("Failed to create cgroup: " + err.Error())
 	}
 
 	// Create a new Memory cgroup and set its limit
 	err = os.WriteFile("/sys/fs/cgroup/memory/mydocker/memory.limit_in_bytes", []byte("50000000"), 0700)
 	if err != nil {
-		panic(err)
+		panic("Failed to set memory limit: " + err.Error())
 	}
 
 	// Add this process to the cgroup
 	pid := strconv.Itoa(os.Getpid())
 	err = os.WriteFile("/sys/fs/cgroup/memory/mydocker/cgroup.procs", []byte(pid), 0700)
 	if err != nil {
-		panic(err)
+		panic("Failed to add process to cgroup: " + err.Error())
 	}
 
 	// Change root filesystem to an isolated filesystem
 	if err := syscall.Chroot("/home/fajri/simple_docker"); err != nil {
-		panic(err)
+		panic("Failed to chroot: " + err.Error())
+	}
+
+	// // Only enable this for debugging purpose
+	// files, err := os.ReadDir("/bin")
+	// if err != nil {
+	// 	panic("Bin not found: " + err.Error())
+	// }
+	// for _, f := range files {
+	// 	fmt.Println(f.Name())
+	// }
+
+	if err := os.Chdir("/"); err != nil {
+		panic("Failed to change directory: " + err.Error())
 	}
 
 	wd, err := os.Getwd()
@@ -60,10 +77,7 @@ func main() {
 	fmt.Println("Current working directory:", wd)
 
 	// Execute a shell within the new namespaces
-	cmd := "/bin/sh"
-	args := []string{"sh"}
-
-	if err := syscall.Exec(cmd, args, os.Environ()); err != nil {
-		panic(err)
+	if err := syscall.Exec("/bin/sh", []string{"sh"}, os.Environ()); err != nil {
+		panic("Failed to exec /bin/sh: " + err.Error())
 	}
 }
